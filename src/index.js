23658919a610c6108efee056448f073e2c0bc9cd
Don Vito Alzaga <dev.dawn024@gmail.com>
2017-11-26 23:29:22 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(
+     <App/>,
+     document.getElementById('root')
+    );
+  });
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
