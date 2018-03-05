0a7181a74e7f72b9d30acad7de45badd9fe83602
Don Vito Alzaga <dev.dawn024@gmail.com>
2018-03-05 14:19:12 +0800

    Add caching support

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,4 +5,47 @@ firebase.initializeApp({
     'messagingSenderId': '85734589405'
 });
 
+const CACHE_NAME = 'v1';
+const CACHE_NAMES = {
+  css: `caa-${VERSION}`,
+  js: `js-${VERSION}`,
+}
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      fetch('asset-manifest-json').then(response => {
+        if (response.ok) {
+          response.json().then(manifest => {
+            const urls = Object.keys(manifest).map(key => manifest[key]);
+            urls.push('/');
+            urls.push('/assets/icon.png');
+            cache.addAll(urls)
+          })
+        }
+      })
+    })
+  )
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  )
+})
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    cache.keys().then(keyList => {
+      Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }))
+    })
+  )
+})
+
 console.log(firebase.messaging());
